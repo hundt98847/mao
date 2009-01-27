3a5e911904b70dbc60f1985366a7c7c3b53870fa
robert.hundt <robert.hundt@60346f97-4a3b-0410-b89e-d778e79482b9>
2009-01-27 17:04:58 +0000

    Added this file.

---
+++ src/MaoOpcodes.cc
@@ -0,0 +1,35 @@
+//
+// Copyright 2008 Google Inc.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, 5th Floor, Boston, MA 02110-1301, USA.
+
+#include "gen-opcodes-table.h"
+
+#include <map>
+
+static bool initialized = false;
+static std::map<const char*, MaoOpcode> name_hash;
+
+MaoOpcode GetOpcode(const char *opcode) {
+  if (!initialized) {
+    int index = 0;
+    while (MaoOpcodeTable[index].name) {
+      name_hash[MaoOpcodeTable[index].name] = MaoOpcodeTable[index].opcode;
+      ++index;
+    }
+    initialized = true;
+  }
+  return name_hash[opcode];
+}
