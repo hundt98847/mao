2a72a65ae4729ceb8bb8cf0cfd5273b889d52e49
robert.hundt <robert.hundt@60346f97-4a3b-0410-b89e-d778e79482b9>
2009-02-18 18:43:50 +0000

    Support for simple DebugActions, to be executed on asserts.

--- trunk/src/MaoDebug.cc
+++ trunk/src/MaoDebug.cc
@@ -24,21 +24,82 @@
 
 // Default output for asserts and traces
 #define MAO_DEFAULT_ASSERT_OUTPUT stderr
+#define ASSERT_PREFIX      "*** ASSERT "
+
 
 FILE *MaoDebug::assert_file_ = MAO_DEFAULT_ASSERT_OUTPUT;
 
+// Debug Action Management.
+MaoDebugAction *debugAction = NULL;
+
+MaoDebugAction::MaoDebugAction() {
+  next_ = debugAction;
+  debugAction = this;
+}
+
+MaoDebugAction::~MaoDebugAction() {
+  if (this == debugAction) {
+    debugAction = next_;
+  } else {
+    MaoDebugAction *action = debugAction;
+    while (action->next()) {
+      if (action->next() == this) {
+        action->set_next(action->next()->next());
+        break;
+      }
+      action = action->next();
+    }
+  }
+}
+
+
+// Source Correlation
+extern "C" {
+  void as_where (char **namep, unsigned int *linep);
+}
+
+class SourceDebugAction : public MaoDebugAction {
+ public:
+  SourceDebugAction() {}
+
+  virtual void Invoke(FILE *output) {
+    char *filename;
+    unsigned int linep;
+    as_where(&filename, &linep);
+
+    if (filename) {
+      fprintf(output, "***   Processing: %s, line: %d\n", filename, linep);
+    }
+  }
+};
+
+static void ProcessDebugActions(FILE *assert_file) {
+  MaoDebugAction *action = debugAction;
+  while (action) {
+    action->Invoke(assert_file);
+    action = action->next();
+  }
+}
+
+// Main Assert routine.
 void MaoDebug::Assert(const char *file_name, int line_number,
                       bool condition, const char *expr_string,
                       const char *format_string, ...) {
   if (! condition) {
-    fprintf(assert_file_, "*** ASSERT! (%s) failed.", expr_string);
-    fprintf(assert_file_, "[%s:%d]\n",
+    fprintf(assert_file_, ASSERT_PREFIX);
+    fprintf(assert_file_, "%s:%d ",
             file_name?file_name:"<unknown>",
             line_number);
+    fprintf(assert_file_, " (%s) failed. ", expr_string);
+
     va_list argList;
     va_start(argList, format_string);
     vfprintf(assert_file_, format_string, argList);
     va_end(argList);
+    fprintf(assert_file_, "\n");
+
+    new SourceDebugAction();
+    ProcessDebugActions(assert_file_);
     abort();
   }
 }
@@ -46,4 +107,3 @@ void MaoDebug::Assert(const char *file_name, int line_number,
 void MaoDebug::SetAssertOutPut(FILE *file) {
   assert_file_ = file;
 }
-
