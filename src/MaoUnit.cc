3de3313cf0b94a072a18172cf0815ab17be0917d
martin.thuresson <martin.thuresson@60346f97-4a3b-0410-b89e-d778e79482b9>
2009-02-27 17:46:48 +0000

    Fixed support for more instruction with rep* prefix.
    Disabled LOOPALIGN by default for now.

--- src/MaoUnit.cc
+++ src/MaoUnit.cc
@@ -1189,7 +1189,12 @@ void InstructionEntry::PrintInstruction(FILE *out) const {
   const MaoOpcode repe_ops[]= {OP_cmps, OP_scas};
   // Opcodes with REPE-bit prefix, but without any prefix in the assembly;
   const MaoOpcode norep_ops[]= {OP_movdqu};
-  const MaoOpcode norepe_ops[]= {OP_movsd};
+  const MaoOpcode norepn_ops[]= {OP_movsd, OP_cvtsi2sd, OP_mulsd, OP_addsd,
+                                 OP_divsd,  OP_subsd, OP_cvttsd2si};
+
+// 
+// OP_cvttsd2si
+// OP_cvttsd2siq
 
   // Prefixes
   fprintf(out, "\t");
@@ -1214,12 +1219,13 @@ void InstructionEntry::PrintInstruction(FILE *out) const {
                                 sizeof(repe_ops)/sizeof(MaoOpcode))) {
               MAO_ASSERT_MSG(false,
                              "Found prefix does not match the instruction.");
-            } else if (IsInList(op(), norepe_ops,
-                                sizeof(norepe_ops)/sizeof(MaoOpcode))) {
+            } else if (IsInList(op(), norepn_ops,
+                                sizeof(norepn_ops)/sizeof(MaoOpcode))) {
 	      ;
             } else {
               MAO_ASSERT_MSG(false,
-                             "Unable to find instruction with rep* prefix");
+                             "Unable to find instruction with repn* "
+                             "prefix for %d", op());
             }
             break;
           case REPE_PREFIX_OPCODE:
@@ -1233,7 +1239,8 @@ void InstructionEntry::PrintInstruction(FILE *out) const {
 	      ;
 	    } else {
               MAO_ASSERT_MSG(false,
-                             "Unable to find instruction with rep* prefix");
+                             "Unable to find instruction with rep* "
+                             "prefix for %d", op());
             }
             break;
             // Rex prefixes are used for 64-bit extention
