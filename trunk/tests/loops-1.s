5c442b61c5f1e40b709bd5e80c4fe8a11a469933
robert.hundt <robert.hundt@60346f97-4a3b-0410-b89e-d778e79482b9>
2009-03-12 17:11:27 +0000

    Simple tester for loop recognition

---
+++ trunk/tests/loops-1.s
@@ -0,0 +1,69 @@
+#Options: -mao:LFIND=lsg
+#grep:    loop-1 2
+        
+      	.file	"hello-loop.c"
+	.text
+.globl foo_with_a_loop
+	.type	foo_with_a_loop, @function
+foo_with_a_loop:
+.LFB2:
+	pushq	%rbp
+.LCFI0:
+	movq	%rsp, %rbp
+.LCFI1:
+	movl	%edi, -20(%rbp)
+	movl	%esi, -24(%rbp)
+	movl	$0, -4(%rbp)
+	jmp	.L2
+.L3:
+	movl	-20(%rbp), %eax
+	addl	%eax, -4(%rbp)
+	subl	$1, -20(%rbp)
+.L2:
+	cmpl	$0, -20(%rbp)
+	jne	.L3
+	movl	-4(%rbp), %eax
+	leave
+	ret
+.LFE2:
+	.size	foo_with_a_loop, .-foo_with_a_loop
+	.section	.eh_frame,"a",@progbits
+.Lframe1:
+	.long	.LECIE1-.LSCIE1
+.LSCIE1:
+	.long	0x0
+	.byte	0x1
+	.string	"zR"
+	.uleb128 0x1
+	.sleb128 -8
+	.byte	0x10
+	.uleb128 0x1
+	.byte	0x3
+	.byte	0xc
+	.uleb128 0x7
+	.uleb128 0x8
+	.byte	0x90
+	.uleb128 0x1
+	.align 8
+.LECIE1:
+.LSFDE1:
+	.long	.LEFDE1-.LASFDE1
+.LASFDE1:
+	.long	.LASFDE1-.Lframe1
+	.long	.LFB2
+	.long	.LFE2-.LFB2
+	.uleb128 0x0
+	.byte	0x4
+	.long	.LCFI0-.LFB2
+	.byte	0xe
+	.uleb128 0x10
+	.byte	0x86
+	.uleb128 0x2
+	.byte	0x4
+	.long	.LCFI1-.LCFI0
+	.byte	0xd
+	.uleb128 0x6
+	.align 8
+.LEFDE1:
+	.ident	"GCC: (Google_crosstoolv12-gcc-4.3.1-glibc-2.3.6-grte) 4.3.1"
+	.section	.note.GNU-stack,"",@progbits
