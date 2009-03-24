2a5f674c54bfc699fe0ead6a4e3f4ac2b661cfe6
martin.thuresson <martin.thuresson@60346f97-4a3b-0410-b89e-d778e79482b9>
2009-03-24 16:39:56 +0000

    Added related files to relaxation tests.

---
+++ trunk/tests/long-jump-space.s
@@ -0,0 +1,14 @@
+#Options:  -mao:RELAX=stat[1]
+#grep MaoRelax.*expr.*134 1
+#
+# On x86_64, the following code, the jump should be 5 bytes, since the offset
+# is longer than 128 bytes.
+#
+.globl expr
+	.type	expr, @function
+expr:
+	jmp	FOO
+	.space 128
+FOO:
+	ret
+	.size	expr, .-expr
---
+++ trunk/tests/relax-function-end.s
@@ -0,0 +1,13 @@
+#Options:  -mao:RELAX=stat[1]
+#grep MaoRelax.*foo.*2 1
+# In order to report the correct size, the align directive should not be
+# part of the function sizes.
+	.type	foo, @function
+foo:	
+	nop
+	nop	
+	.size foo, .-foo
+	.align 2
+	.type	bar, @function
+bar:
+	nop
