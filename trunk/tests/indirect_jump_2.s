564a6baa6a1c4540a9f76f943997b0b2fd7e6ad9
martin.thuresson <martin.thuresson@60346f97-4a3b-0410-b89e-d778e79482b9>
2009-03-31 20:43:19 +0000

    Added test for another jump-table pattern.

---
+++ trunk/tests/indirect_jump_2.s
@@ -0,0 +1,31 @@
+#Option:  -mao:LOOPALIGN=enable -mao:CFG=stat[1] -mao:-o/dev/null
+#grep table.patterns\:.* 1 1
+#
+# The following pattern should be identified as a jump table.
+# Here, the table needs to be parsed in order to get the correct
+# Label targets
+
+        .text
+.globl main
+        .type   main, @function
+main:
+	cmpl	$28, %eax
+	ja	.L352
+	leaq	.L385(%rip), %rdi
+	mov	%eax, %eax
+	movslq	(%rdi,%rax,4),%rax
+	addq	%rdi, %rax
+	jmp	*%rax
+	.section	.rodata
+	.align 4
+.L385:
+	.long	.L356-.L385
+	.long	.L664-.L385
+	.long	.L665-.L385
+	.long	.L666-.L385
+	.long	.L617-.L385
+	.long	.L618-.L385
+	.long	.L619-.L385
+	.text
+	ret
+	.size	main, .-main
