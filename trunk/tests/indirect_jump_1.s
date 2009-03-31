15917aa76ff4d3c3ba1f6eaa3e2d0667b00fe613
martin.thuresson <martin.thuresson@60346f97-4a3b-0410-b89e-d778e79482b9>
2009-03-31 16:44:40 +0000

    Added a new pattern for indirect jumps that is currently not handled in MAO.

---
+++ trunk/tests/indirect_jump_1.s
@@ -0,0 +1,48 @@
+#Option:  -mao:LOOPALIGN=enable -mao:CFG=stat[1] -mao:-o/dev/null
+#grep table.patterns\:.* 1 1
+#
+# The following pattern should be identified as a jump table.
+# Here, the table needs to be parsed in order to get the correct
+# Label targets
+
+        .text
+.globl main
+        .type   main, @function
+main:
+        pushq   %rbp
+        movq    %rsp, %rbp
+
+	movq    $2, %rdi
+
+	leaq	TABLE(%rip), %rdx
+	mov	%edi, %eax
+	movslq	(%rdx,%rax,4),%rax
+	addq	%rdx, %rax
+	jmp	*%rax
+	.section	.rodata
+	.align 4
+	.align 4
+TABLE:
+	.long	L8-TABLE
+	.long	L6-TABLE
+	.long	L5-TABLE
+	.long	L6-TABLE
+	.long	L7-TABLE
+	.long	L8-TABLE
+	.long	L8-TABLE
+	.long	L10-TABLE
+	.text
+L5:
+	nop	
+L6:
+	nop
+L7:
+	nop
+L8:
+	nop
+L9:
+	nop
+L10:
+        movl    $0, %eax
+        leave
+        ret
