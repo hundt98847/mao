81ef8ae842a83ad13d81e6463450f8935c8ecd96
caseyburkhardt@google.com <caseyburkhardt@google.com@60346f97-4a3b-0410-b89e-d778e79482b9>
2009-07-09 23:42:05 +0000

    Adding directory for Instruction Analysis Tool within /src and adding code for first iteration of the Test Generator

---
+++ trunk/src/IAT/Assembly.cpp
@@ -0,0 +1,98 @@
+//
+// Copyright 2009 and later Google Inc.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the
+//   Free Software Foundation Inc.,
+//   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+// Author: Casey Burkhardt (caseyburkhardt)
+// Description: File serves as the primary implementation for the Assembly
+//              class, which represents the abstracted version of an assembly
+//              file to be written to the file system by the test generator.
+//
+
+#include <string>
+
+#include "./Assembly.h"
+
+// Constructor
+Assembly::Assembly() {
+  this->instruction_body_.clear();
+  this->addressing_mode_.clear();
+  this->instruction_name_.clear();
+  this->file_name_.clear();
+  this->generation_complete_ = false;
+  this->output_complete_ = false;
+}
+
+// Destructor
+Assembly::~Assembly() {
+  // TODO(caseyburkhardt): Implement Destructor
+}
+
+void Assembly::append_instructions(string data) {
+  this->instruction_body_.append(data);
+  this->instruction_body_.append("\n");
+}
+
+string Assembly::instruction_body() {
+  return this->instruction_body_;
+}
+
+void Assembly::set_instruction_name(string data) {
+  this->instruction_name_ = data;
+}
+
+string Assembly::instruction_name() {
+  return this->instruction_name_;
+}
+
+void Assembly::set_addressing_mode(string data) {
+  this->addressing_mode_ = data;
+}
+
+string Assembly::addressing_mode() {
+  return this->addressing_mode_;
+}
+
+void Assembly::set_file_name(string data) {
+  this->file_name_ = data;
+}
+
+string Assembly::file_name() {
+  return this->file_name_;
+}
+
+void Assembly::set_generation_complete(bool status) {
+  this->generation_complete_ = status;
+}
+
+bool Assembly::generation_complete() {
+  return this->generation_complete_;
+}
+
+void Assembly::set_output_complete(bool status) {
+  this->output_complete_ = status;
+}
+
+bool Assembly::output_complete() {
+  return this->output_complete_;
+}
+
+string Assembly::OutputString() {
+  string result = "Instruction Name: " + this->instruction_name_ + "\n";
+  result = result + "Addressing Mode: " + this->addressing_mode_ + "\n";
+  result = result + "Instruction Body: \n" + this->instruction_body_;
+  return result;
+}
