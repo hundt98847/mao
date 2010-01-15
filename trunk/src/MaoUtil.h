b5b612bd765dea991304d9488028f9a5efc0d831
eraman+personal@google.com <eraman+personal@google.com@60346f97-4a3b-0410-b89e-d778e79482b9>
2010-01-15 20:02:37 +0000

    Guard declaration of bits_per_word.

--- trunk/src/MaoUtil.h
+++ trunk/src/MaoUtil.h
@@ -294,10 +294,11 @@ class BitString {
 
   void VerifyBitString() {
     // Check relation between number_of_bits_ and number_of_words_.
+#ifdef NDEBUG
     int bits_per_word = sizeof(unsigned long long) * 8;
     MAO_ASSERT(((number_of_words_ * bits_per_word) >= number_of_bits_)
                && (((number_of_words_ - 1) * bits_per_word) < number_of_bits_));
-
+#endif
     if (!IsUndef()) {
       // Assert that all unused bits are zero.
       int unused_bits = sizeof(unsigned long long)*8 -
