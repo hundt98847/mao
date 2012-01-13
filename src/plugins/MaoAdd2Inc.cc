b824b7deb6e9b95435d0e8be8c122c52452b0478
robert.hundt <robert.hundt@60346f97-4a3b-0410-b89e-d778e79482b9>
2012-01-13 01:38:23 +0000

    New pass to convert add|sub 1,reg to inc|dec reg

---
+++ src/plugins/MaoAdd2Inc.cc
@@ -0,0 +1,127 @@
+//
+// Copyright 2012 Google Inc.
+//
+// This program is free software; you can redistribute it and/or to
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the
+//   Free Software Foundation, Inc.,
+//   51 Franklin Street, Fifth Floor,
+//   Boston, MA  02110-1301, USA.
+
+// Convert add|sub -1|1,reg to inc|dec reg (the reverse is
+// done in MaoInc2Add.cc)
+//
+// Note that there is a subtle dependence which is not being
+// handled by this pass.
+//
+//  inc/dec only write a subset of the flag registers
+//  add/sub overwrite all flags.
+//
+//  inc/dec therefore introduce a dependence on previous
+//  writes to the flags register.
+//
+// This is not handled in this pass, assumption is that
+// compilers won't model the flags at this level of granularity
+// anyways, so this is more a theoreritical concern.
+//
+#include "Mao.h"
+
+namespace {
+PLUGIN_VERSION
+
+// --------------------------------------------------------------------
+// Options
+// --------------------------------------------------------------------
+MAO_DEFINE_OPTIONS(ADD2INC, "Convert add|sub 1,reg to inc|dec reg", 0)
+  {};
+
+// --------------------------------------------------------------------
+// Pass
+// --------------------------------------------------------------------
+class Add2IncPass : public MaoFunctionPass {
+ public:
+  Add2IncPass(MaoOptionMap *options, MaoUnit *mao, Function *function)
+    : MaoFunctionPass("ADD2INC", options, mao, function) { }
+
+  // Look for these patterns:
+  //     add/sub 1, reg
+  // where reg can be any register specification, e.g., ah, al, ax, eax, rax
+  // for whichever registers support these forms.
+  //
+  bool Go() {
+    CFG *cfg = CFG::GetCFG(unit_, function_);
+
+    // Container for instructions to remove. In order
+    // to make things simple and to avoid messing up
+    // iterators, we collect instructions in a first
+    // pass and delete them in a second loop.
+    //
+    std::list<InstructionEntry *> redundants;
+
+    // Iterate over all BBs, all entries which are instructions.
+    // Find instructions that have 2 operands, an immediate as
+    // the 1st operand, and a regiser as the second operand.
+    // Then, if the immediate is 1 and opcodes are OP_add or
+    // OP_sub, replace the instructions with an inc or dec
+    // instruction.
+    //
+    FORALL_CFG_BB(cfg, it) {
+      FORALL_BB_ENTRY(it, entry) {
+        if (!(*entry)->IsInstruction()) continue;
+        InstructionEntry *insn = (*entry)->AsInstruction();
+
+        if (insn->NumOperands() != 2 ||
+            !insn->IsImmediateIntOperand(0) ||
+            !insn->IsRegisterOperand(1))
+          continue;
+
+        if (insn->op() == OP_add && insn->GetImmediateIntValue(0) == 1) {
+          InstructionEntry *i = unit_->CreateIncFromOperand(function_, insn, 1);
+          insn->LinkBefore(i);
+          redundants.push_back(insn);
+          TraceReplace(insn, i);
+        }
+        if (insn->op() == OP_sub && insn->GetImmediateIntValue(0) == 1) {
+          InstructionEntry *i = unit_->CreateDecFromOperand(function_, insn, 1);
+          insn->LinkBefore(i);
+          redundants.push_back(insn);
+          TraceReplace(insn, i);
+        }
+      }
+    }
+
+    // Now delete all the collected redundant instructions.
+    for (std::list<InstructionEntry *>::iterator it = redundants.begin();
+         it != redundants.end(); ++it) {
+      unit_->DeleteEntry(*it);
+    }
+
+    return true;
+  }
+
+ private:
+  // Helper function to dump modified insn's.
+  void TraceReplace(InstructionEntry *before, InstructionEntry *after) {
+    TraceC(1, "*** Replaced: ");
+    if (tracing_level() > 0) before->PrintEntry(stderr);
+    TraceC(1, "*** With    : ");
+    if (tracing_level() > 0) after->PrintEntry(stderr);
+  }
+};
+
+extern "C" {
+  void MaoInit() {
+    REGISTER_FUNC_PASS("ADD2INC", Add2IncPass )
+  }
+}
+
+} // namespace
